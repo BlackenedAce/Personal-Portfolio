d888a6c3b52457d1958ad4509face7d88309f434
Ghandi Matthew <ghandi.j.matthew@gmail.com>
2022-05-17 21:38:19 +0800

    Update main.js
    Added a fifth variable for the fifth project to be hiddeb

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ const three = document.querySelector("#three");
 
 const four = document.querySelector("#four");
 
+const five = document.querySelector("#five");
+
 function showAndHide () {
   if (pointer == 0) {
     pointer= 1;
@@ -19,6 +21,7 @@ function showAndHide () {
   two.style.display= "inherit"
   three.style.display= "inherit"
     four.style.display= "inherit"
+    five.style.display= "inherit"
   }
   else if (pointer == 1) {
     pointer = 0;
@@ -27,6 +30,7 @@ function showAndHide () {
   two.style.display= "none"
   three.style.display= "none"
     four.style.display= "none"
+    five.style.display= "none"
   }
 }
 button.addEventListener("click",showAndHide);
